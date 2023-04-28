e5981b95727678152b7c6556538d4f1351a272e3
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-04-28 10:58:11 +0700

    Create starter.java

---
+++ src/ft/starter.java
@@ -0,0 +1,38 @@
+package ft;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import ft.blocks.FlareBlocks;
+
+public class starter extends Mod{
+
+    public starter(){
+        Log.info("Loaded  constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("hi");
+                dialog.cont.add("trauma").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("trauma")).pad(20f).row();
+                dialog.cont.button("ok", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        new FlareBlocks().load();
+        Log.info("Loading content.");
+    }
+
+}
