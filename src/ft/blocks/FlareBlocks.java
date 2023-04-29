4e79be5d81fd8350ba7781de97d04bc0119a9f53
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-04-29 10:02:28 +0700

    Update FlareBlocks.java

--- src/ft/blocks/FlareBlocks.java
+++ src/ft/blocks/FlareBlocks.java
@@ -37,7 +37,7 @@ import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 import mindustry.content.StatusEffects;
 import mindustry.content.Items;
-import mindustry.content.Fx;
+import mindustry.core.Sound;
 
 import static mindustry.Vars.*;
 import static mindustry.type.ItemStack.*;
