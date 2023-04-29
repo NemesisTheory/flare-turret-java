4c75343496c09846c30a7ed29abb52d6fe309be9
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-04-29 14:51:31 +0700

    Update FlareBlocks.java

--- src/ft/blocks/FlareBlocks.java
+++ src/ft/blocks/FlareBlocks.java
@@ -1,52 +1,38 @@
 package ft.blocks;
 
 import arc.graphics.*;
+import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.math.geom.*;
 import arc.struct.*;
-import mindustry.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
 import mindustry.entities.*;
 import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
 import mindustry.entities.effect.*;
-import mindustry.entities.part.DrawPart.*;
 import mindustry.entities.part.*;
 import mindustry.entities.pattern.*;
+
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.type.ammo.*;
 import mindustry.type.unit.*;
-import mindustry.world.*;
-import mindustry.world.blocks.*;
-import mindustry.world.blocks.campaign.*;
-import mindustry.world.blocks.defense.*;
-import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.heat.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.liquid.*;
-import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
-import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
+import mindustry.type.weapons.*;
 import mindustry.world.meta.*;
-import mindustry.content.StatusEffects;
-import mindustry.content.Items;
-import mindustry.content.Fx;
-import mindustry.core.assets.Sounds;
 
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
 import static mindustry.Vars.*;
-import static mindustry.type.ItemStack.*;
+import mindustry.content.*;
 
 public class FlareBlocks {
     public static ItemTurret reverence;
+
     public static void load() {
-        
         reverence = new ItemTurret("Reverence") {{
             requirements(Category.turret, with(Items.copper, 820, Items.graphite, 655, Items.titanium, 650, Items.silicon, 455, Items.thorium, 250));
             ammo(
@@ -62,7 +48,6 @@ public class FlareBlocks {
                         collidesAir = true;
                         hitEffect = Fx.colorSparkBig;
                         smokeEffect = Fx.fireSmoke;
-                        shootEffect = Fx.shootBig;
                         despawnEffect = Fx.fire;
                         pierce = false;
                         incendAmount = 1;
@@ -72,6 +57,7 @@ public class FlareBlocks {
                         hittable = true;
                         hitEffect = new MultiEffect(Fx.hitBulletSmall, Fx.fireHit);
                         makeFire = true;
+                        shootEffect = Fx.shootBig;
                         ammoMultiplier = 3;
                         splashDamage = 185f;
                         splashDamageRadius = 86f;
@@ -88,7 +74,7 @@ public class FlareBlocks {
             shake = 1.5f;
             size = 3;
             shootCone = 18f;
-            shootSound = Sounds.shootBig;
+            shootSound = Fx.shootBig;
 
             scaledHealth = 145;
             coolant = consumeCoolant(1f);
