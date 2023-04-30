78191436b972e220599fdca82590fa75925df599
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-04-30 19:33:33 +0700

    Update FlareItems.java

--- src/ft/content/FlareItems.java
+++ src/ft/content/FlareItems.java
@@ -19,32 +19,43 @@ public class FlareItems {
     public static Item flareItem;
     public static Item flarogusItem;
     public static Item susgeAlloy;
+    public static Item shit;
 
     public static void load() {
         flareItem = new Item("flare-item") {{
             cost = 1.2f;
             hardness = 6;
-            radioactivity = 5000f;
-            explosiveness = 5000f;
+            radioactivity = 12f;
+            explosiveness = 50f;
             flammability = 0.01f;
-            charge = 5000f;
+            charge = 0.4f;
         }};
 
         susgeAlloy = new Item("susge-alloy") {{
             cost = 1.4f;
             hardness = 1;
-            radioactivity = 21474837f;
-            explosiveness = 21474837f;
+            radioactivity = 13f;
+            explosiveness = 0.4f;
             flammability = 0.2f;
-            charge = 21474837f;
+            charge = 153f;
         }};
 
         flarogusItem = new Item("flarogus-item") {{
             cost = 1.4f;
             hardness = 5;
-            radioactivity = 21474837f;
-            explosiveness = 21474837f;
+            radioactivity = 500f;
+            explosiveness = 12f;
             flammability = 0.01f;
-            charge = 21474837f;
+            charge = 100f;
         }};
-    }}
+
+        shit = new Item("shit") {{
+            cost = 1.4f;
+            hardness = 10;
+            radioactivity = 300000f;
+            explosiveness = 573278388f;
+            flammability = 0.5f;
+            charge = 534f;
+        }};
+    }
+}
