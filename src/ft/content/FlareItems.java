a11ba07230d298d5e83c13f173cbce166399b2f4
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-04-30 13:31:22 +0700

    Update FlareItems.java

--- src/ft/content/FlareItems.java
+++ src/ft/content/FlareItems.java
@@ -1,21 +1,21 @@
-package ft.items;
+package ft.content;
 
 import arc.graphics.Color;
-import mindustry.type.Item;
 import mindustry.content.Items;
+import mindustry.type.Item;
 
 public class FlareItems {
-public static Item flareItem;
+    public static Item flareItem;
 
     public static void load() {
-        flareItem = new Item("flare-item") {;
-                cost = 1.2;
+        flareItem = new Item("flare-item") {{
+                cost = 1.2f;
                 hardness = 6;
                 radioactivity = 5000f;
                 explosiveness = 5000f;
                 flammability = 0.01f;
                 charge = 5000f;
-            };
+            }
         };
     }
 }
