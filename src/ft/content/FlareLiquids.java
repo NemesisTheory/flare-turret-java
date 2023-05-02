358303e2b0bacb37d898c2df825e93f147d7c170
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-05-02 17:03:20 +0700

    shave

--- src/ft/content/FlareLiquids.java
+++ src/ft/content/FlareLiquids.java
@@ -1,25 +1,8 @@
 package ft.content;
 
-import arc.graphics.Color;
 import mindustry.type.Liquid;
 import mindustry.graphics.Pal;
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.struct.*;
-import arc.util.*;
-import mindustry.content.*;
-import mindustry.ctype.*;
-import mindustry.entities.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.logic.*;
-import mindustry.world.*;
-import mindustry.world.meta.*;
-import mindustry.content.Liquids;
-import mindustry.content.*;
 
-import static mindustry.entities.Puddles.*;
 import static mindustry.content.Liquids.*;
 
 public class FlareLiquids {
